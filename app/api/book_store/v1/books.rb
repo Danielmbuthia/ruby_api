1dd93018739d8df39320ac6f74479f99b5f3d1d1
Daniel Mbuthia <dambuthia@outlook.com>
2021-11-09 16:48:59 +0300

    grape entities

--- app/api/book_store/v1/books.rb
+++ app/api/book_store/v1/books.rb
@@ -8,8 +8,31 @@ module BookStore
           desc 'Return list of books'
           get do
             books = Book.all
-            present books
+            present BookStore::Entities::Book.represent(books)
           end
+
+          desc 'Return one specific book'
+          route_param :id do
+            get do
+              book = Book.find(params[:id])
+              present BookStore::Entities::Book.represent(book)
+            end
+            resource :flows do
+              desc 'Create a flow.'
+               params do
+                 requires :flow, type: Hash do
+                 requires :newStock, type: Integer, desc: 'New Stock.'
+                 requires :previousStock, type: Integer, desc: 'Previous Stock.'
+                end
+               end
+               post do
+                @book = Book.find(params[:id])
+                @flow = Flow.new(params[:flow])
+                @flow = @book.flows.create!(params[:flow])
+                @book.update(stock: @flow.newStock)
+               end
+              end
+            end
         end
       end
     end
